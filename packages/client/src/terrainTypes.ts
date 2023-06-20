ea7093e349d248b9470e00d86d87f4e870dd5dde
yukiorita1117 <3204704@gmail.com>
2023-06-20 22:33:47 +0900

    fix terrain type

--- packages/client/src/terrainTypes.ts
+++ packages/client/src/terrainTypes.ts
@@ -1,17 +1,25 @@
 export enum TerrainType {
-  TallGrass = 1,
+  Grass = 1,
+  Tree,
   Boulder,
+  HasItem,
 }
 
 type TerrainConfig = {
-  emoji: string;
+  emoji: any;
 };
 
 export const terrainTypes: Record<TerrainType, TerrainConfig> = {
-  [TerrainType.TallGrass]: {
-    emoji: "🌳",
+  [TerrainType.Grass]: {
+    emoji: 'assets/glass.png',
+  },
+  [TerrainType.Tree]: {
+    emoji: 'assets/tree.png',
   },
   [TerrainType.Boulder]: {
-    emoji: "🪨",
+    emoji: 'assets/boulder.png',
+  },
+  [TerrainType.HasItem]: {
+    emoji: 'assets/glass.png',
   },
 };
