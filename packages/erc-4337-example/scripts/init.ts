c25f579cbb1482d29c72c91d245fa5898ce107d2
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-06-25 14:10:32 +0900

    READMEにPaymasterを使って実行させた記録を記載しました。

--- packages/erc-4337-example/scripts/init.ts
+++ packages/erc-4337-example/scripts/init.ts
@@ -6,12 +6,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const {
-  API_KEY 
+  API_KEY,
+  PRIVATE_KEY
 } = process.env;
 
 const INIT_CONFIG = {
   rpcUrl: `https://api.stackup.sh/v1/node/${API_KEY}`,
-  signingKey: new ethers.Wallet(ethers.utils.randomBytes(32)).privateKey,
+  signingKey: PRIVATE_KEY,
   entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
   simpleAccountFactory: "0x9406Cc6185a346906296840746125a0E44976454",
   //simpleAccountFactory: "0x398004f2698648159b8e82a13fd87ea0b1f8f84e",
